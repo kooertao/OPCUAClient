83ebd66806c502d4251ceec4f0dcf7279eb93b13
Tao, Steve (Shanghai) <kooertao@163.com>
2017-07-27 11:16:27 +0800

    OPC UA Client

---
+++ OPCUAClient/MainFrm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace OPCUAClient
+{
+   partial class MainFrm
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.CreateSessionBtn = new System.Windows.Forms.Button();
+         this.AddSubscriptionBtn = new System.Windows.Forms.Button();
+         this.BrowseBtn = new System.Windows.Forms.Button();
+         this.tvItems = new System.Windows.Forms.TreeView();
+         this.lvItems = new System.Windows.Forms.ListView();
+         this.ReadNode = new System.Windows.Forms.Button();
+         this.tbResult = new System.Windows.Forms.TextBox();
+         this.SuspendLayout();
+         // 
+         // CreateSessionBtn
+         // 
+         this.CreateSessionBtn.Location = new System.Drawing.Point(13, 14);
+         this.CreateSessionBtn.Name = "CreateSessionBtn";
+         this.CreateSessionBtn.Size = new System.Drawing.Size(121, 23);
+         this.CreateSessionBtn.TabIndex = 1;
+         this.CreateSessionBtn.Text = "Create Session";
+         this.CreateSessionBtn.UseVisualStyleBackColor = true;
+         this.CreateSessionBtn.Click += new System.EventHandler(this.CreateSessionBtn_Click);
+         // 
+         // AddSubscriptionBtn
+         // 
+         this.AddSubscriptionBtn.Location = new System.Drawing.Point(13, 64);
+         this.AddSubscriptionBtn.Name = "AddSubscriptionBtn";
+         this.AddSubscriptionBtn.Size = new System.Drawing.Size(121, 23);
+         this.AddSubscriptionBtn.TabIndex = 2;
+         this.AddSubscriptionBtn.Text = "Add Subscription";
+         this.AddSubscriptionBtn.UseVisualStyleBackColor = true;
+         this.AddSubscriptionBtn.Click += new System.EventHandler(this.AddSubscriptionBtn_Click);
+         // 
+         // BrowseBtn
+         // 
+         this.BrowseBtn.Location = new System.Drawing.Point(13, 117);
+         this.BrowseBtn.Name = "BrowseBtn";
+         this.BrowseBtn.Size = new System.Drawing.Size(121, 24);
+         this.BrowseBtn.TabIndex = 3;
+         this.BrowseBtn.Text = "Browse";
+         this.BrowseBtn.UseVisualStyleBackColor = true;
+         this.BrowseBtn.Click += new System.EventHandler(this.ReadNodeBtn_Click);
+         // 
+         // tvItems
+         // 
+         this.tvItems.Location = new System.Drawing.Point(140, 117);
+         this.tvItems.Name = "tvItems";
+         this.tvItems.Size = new System.Drawing.Size(250, 197);
+         this.tvItems.TabIndex = 4;
+         // 
+         // lvItems
+         // 
+         this.lvItems.Location = new System.Drawing.Point(408, 117);
+         this.lvItems.Name = "lvItems";
+         this.lvItems.Size = new System.Drawing.Size(287, 136);
+         this.lvItems.TabIndex = 5;
+         this.lvItems.UseCompatibleStateImageBehavior = false;
+         // 
+         // ReadNode
+         // 
+         this.ReadNode.Location = new System.Drawing.Point(13, 344);
+         this.ReadNode.Name = "ReadNode";
+         this.ReadNode.Size = new System.Drawing.Size(75, 23);
+         this.ReadNode.TabIndex = 6;
+         this.ReadNode.Text = "Read";
+         this.ReadNode.UseVisualStyleBackColor = true;
+         this.ReadNode.Click += new System.EventHandler(this.ReadNode_Click);
+         // 
+         // tbResult
+         // 
+         this.tbResult.Location = new System.Drawing.Point(140, 346);
+         this.tbResult.Multiline = true;
+         this.tbResult.Name = "tbResult";
+         this.tbResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+         this.tbResult.Size = new System.Drawing.Size(250, 134);
+         this.tbResult.TabIndex = 7;
+         // 
+         // MainFrm
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(755, 509);
+         this.Controls.Add(this.tbResult);
+         this.Controls.Add(this.ReadNode);
+         this.Controls.Add(this.lvItems);
+         this.Controls.Add(this.tvItems);
+         this.Controls.Add(this.BrowseBtn);
+         this.Controls.Add(this.AddSubscriptionBtn);
+         this.Controls.Add(this.CreateSessionBtn);
+         this.Name = "MainFrm";
+         this.Text = "UAClient";
+         this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainFrm_FormClosed);
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+      private System.Windows.Forms.Button CreateSessionBtn;
+      private System.Windows.Forms.Button AddSubscriptionBtn;
+      private System.Windows.Forms.Button BrowseBtn;
+      private System.Windows.Forms.TreeView tvItems;
+      private System.Windows.Forms.ListView lvItems;
+      private System.Windows.Forms.Button ReadNode;
+      private System.Windows.Forms.TextBox tbResult;
+   }
+}
+
---
+++ OPCUAClient/MainFrm.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Advosol.EasyUA;
+using Opc.Ua;
+
+namespace OPCUAClient
+{
+   public partial class MainFrm : Form
+   {
+      private UaClient uaApp;
+      private Session session;
+      private ShowBrowseTreeList sbtl = null;
+      private const string UAServerAddress = "opc.tcp://localhost:62841/Advosol/uaPLUS";
+      public MainFrm()
+      {
+         InitializeComponent();
+         try
+         {
+            UaClient.UaAppConfigFileAutoCreate = "OPCUAClient";   // auto create a default UA config file to simplify the configuration handling
+            uaApp = new UaClient(this);
+            uaApp.LoadConfiguration();    // process the UA configuration
+         }
+         catch(Exception ex)
+         {
+            MessageBox.Show(ex.Message, "Load UA configuration file failed.");
+         }
+      }
+
+      private void ReadNodeBtn_Click(object sender, EventArgs e)
+      {
+         if (session != null)
+         {
+            try
+            {
+               if (sbtl != null)    // previous treeList instance
+               {
+                  sbtl.Dispose();
+                  sbtl = null;
+               }
+
+               lvItems.Items.Clear();
+               tvItems.Nodes.Clear();
+               sbtl = new ShowBrowseTreeList(session, tvItems, lvItems);
+               if (sbtl != null)
+               {
+                  sbtl.Show("UaServer");
+               }
+
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show(ex.Message, "Browse failed.");
+            }
+         }
+      }
+
+      private void AddSubscriptionBtn_Click(object sender, EventArgs e)
+      {
+         if (session != null)
+         {
+            session.SubscriptionRequestKeepaliveCount = 5;
+            session.PublishNotification += new OnNotification(onSessionPublishNotification);
+            try
+            {
+
+               var subscr = session.AddSubscription("s1", 500);
+
+               subscr.PublishStatusChanged += new EventHandler(onSubscrPublishStatusChanged);
+               //subscr.DataChangeCallback += onSubscrDataChangeNotification;
+
+               AddSubscriptionBtn.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show(ex.Message, "Add Subscription failed.");
+            }
+         }
+      }
+
+      void onSessionPublishNotification(Session session, NotificationEventArgs args)
+      {
+         //MessageBox.Show("Session publish notification");
+      }
+      void onSubscrPublishStatusChanged(object sender, EventArgs e)
+      {
+         PublishStatusChangedArgs args = (PublishStatusChangedArgs)e;
+         MessageBox.Show("Status changed: " + args.newState);
+      }
+
+      private void CreateSessionBtn_Click(object sender, EventArgs e)
+      {
+         try
+         {
+            session = uaApp.CreateSession(UAServerAddress, false, "sessionForUAClient");
+            session.ReconnectPeriod = 10000;
+            session.SessionTimeout = 60000;
+            session.StatusChange += new OnStatusChange(onSessionStatusChange);
+            //session.NotifyUntrustedCertificate += new OnNotifyUntrustedCertificate(onSessionNotifyUntrustedCertificate);
+            session.NamespaceIndexManagement = true;
+
+            session.Connect(null);
+
+            //btnCreateSession.Enabled = false;
+            //btnTerminateSession.Enabled = true;
+            //btnAddSubscripton.Enabled = true;
+            //btnDeleteSubscription.Enabled = true;
+            //gbSession.Enabled = true;
+         }
+         catch (Exception ex)
+         {
+            MessageBox.Show(ex.Message, "Create session failed.");
+         }
+      }
+
+      void onSessionStatusChange(Session session, StatusCheckEventArgs e)
+      {
+         try
+         {
+            if (e.Status != null)
+            {
+               string state = e.CurrentState.ToString();
+               //tbServerState.Text = state;
+               //tbNotifications.Text = e.CurrentTime.ToString() + "  " + e.Status.LocalizedText.Text;
+            }
+            else
+            {
+               string state = e.CurrentState.ToString();
+              // tbServerState.Text = state;
+            }
+         }
+         catch (Exception ex)
+         {
+            //tbNotifications.Text = "Status Change  " + ex.Message;
+         }
+      }
+
+      private void ReadNode_Click(object sender, EventArgs e)
+      {
+         if (session != null)
+         {
+            try
+            {
+               List<NodeId> selNodes = new List<NodeId>();
+               selNodes = sbtl.GetSelectedListViewNodes();
+
+               if (selNodes.Count == 0)
+                  MessageBox.Show("No Item Node Selected.");
+               else
+               {
+                  tbResult.Text = "reading ...";
+                  List<ReadValueId> nodesToRead = new List<ReadValueId>();
+                  foreach (NodeId selNode in selNodes)
+                  {
+                     ReadValueId rv = new ReadValueId();
+                     rv.NodeId = selNode;
+                     rv.AttributeId = Attributes.Value;
+                     nodesToRead.Add(rv);
+                  }
+
+                  List<DataValue> rslt = session.Read(nodesToRead);
+
+                  tbResult.Text = "";
+                  for (int i = 0; i < rslt.Count; ++i)
+                  {
+                     DataValue r = rslt[i];
+                     {
+                        string val = "null";
+                        if (r.Value != null)
+                           val = r.Value.ToString();
+                        tbResult.Text += val + "   " + r.StatusCode.ToString() + Environment.NewLine;
+                     }
+                  }
+               }
+            }
+            catch (Exception ex)
+            {
+               tbResult.Text = "";
+               MessageBox.Show(ex.Message, "Read Value failed.");
+            }
+         }
+      }
+
+      private void MainFrm_FormClosed(object sender, FormClosedEventArgs e)
+      {
+         try
+         {
+            if (session != null)
+            {
+               session.Close();    // terminate the open session
+               session.Dispose();
+            }
+         }
+         catch { }
+      }
+   }
+}
